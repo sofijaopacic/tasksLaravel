4f2b66bc2b1cd5113b12a7f3ef8dae6f73f57275
sofijaopacic <opacic.sofija@gmail.com>
2022-08-25 00:13:08 +0200

    Kreirane migracije

---
+++ database/migrations/2022_08_24_220654_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('description');
+            $table->string('due_date');
+            $table->boolean('finished');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
